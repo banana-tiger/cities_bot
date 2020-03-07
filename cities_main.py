86429635ab32ff8a99d47a9c7eb4f9fddb7d17c2
Anton Maliutin <amalutin13@gmail.com>
2020-03-08 00:46:08 +0300

    основной модуль игры в города

--- cities_main.py
+++ cities_main.py
@@ -1,30 +1,50 @@
-import string
-
+import csv
 
 def main():
-    test_list = ['архангельск', 'астрахань', 'москва', 'норильск', 'калуга']
+    with open('city.csv', 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        cities_list = [row['address'].split('г ')[-1].lower() for row in reader]
+
+    
     used_list = []
+    trash_letters_list = ['ь', 'ъ', 'ы']
     last_letter = ''
 
 
     while True:
-        input_text = input()
-
+        input_text = input().lower()
 
         if not used_list:
-            if input_text not in test_list:
+            if input_text not in cities_list:
                 print('Введенного города нет в списке')
             else:
                 used_list.append(input_text)
-                last_letter = input_text[-1]
+                if input_text[-1] in trash_letters_list:
+                    for letter in input_text[::-1]:
+                        if letter in trash_letters_list:
+                            continue
+                        else:
+                            last_letter = letter
+                            break        
+                else:
+                    last_letter = input_text[-1]
         else:
-            if input_text not in test_list:
+            if input_text not in cities_list:
                 print('Введенного города нет в списке')
             elif last_letter != input_text[0]:
-                print('Первая буква введенного города не соответствует последней букве предыдущего')
+                print('Первая буква введенного города не соответствует последней букве предыдущего либо город уже игрался')
             else:
                 used_list.append(input_text)
-                last_letter = input_text[-1]
+                if input_text[-1] in trash_letters_list:
+                    for letter in input_text[::-1]:
+                        if letter in trash_letters_list:
+                            continue
+                        else:
+                            last_letter = letter
+                            break
+                else:
+                    last_letter = input_text[-1]
+                        
 
         print(used_list, last_letter)
 
